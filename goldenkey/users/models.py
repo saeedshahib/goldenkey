eaa10925f0ca8e6d75cd95b2c824d63899ffb3a7
Saeed Shahib Zadeh <saeedshahib@gmail.com>
2021-12-21 21:34:41 +0330

    users

---
+++ goldenkey/users/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.translation import ugettext_lazy as _
+from .managers import CustomUserManager
+# Create your models here.
+
+
+
+#User model for our customers
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+	firstName = models.CharField(max_length=100,null=True,blank=True)
+	lastName = models.CharField(max_length=100,null=True,blank=True)
+	email = models.EmailField(_('email address'), unique=True)
+	profilePhoto = models.ImageField(upload_to='userprofiles/',blank=True)
+	date_joined = models.DateTimeField(auto_now_add=True)
+	is_staff = models.BooleanField(default=False)
+	is_active = models.BooleanField(default=True)
+
+	USERNAME_FIELD = 'email'
+	REQUIRED_FIELDS = []
+
+	objects = CustomUserManager()
+	
+
+	def __str__(self):
+		return self.email
---
+++ goldenkey/users/serializers.py
@@ -0,0 +1,45 @@
+from rest_framework import serializers
+from .models import CustomUser
+from rest_auth.registration.serializers import RegisterSerializer
+
+#Serializer for CustomUser Model
+
+class CustomUserSerializer(serializers.ModelSerializer):
+	firstName = serializers.CharField(required = False)
+	lastName = serializers.CharField(required = False)
+	email = serializers.EmailField(required = False)
+	profilePhoto = serializers.ImageField(required = False)
+
+	class Meta:
+		model = CustomUser
+		fields = ['id', 'firstName','lastName','email','profilePhoto']
+
+
+
+#Register Serializer for registering our users
+
+class MyCustomUserRegistrationSerializer(RegisterSerializer):
+	firstName = serializers.CharField(required = False)
+	lastName = serializers.CharField(required = False)
+	email = serializers.EmailField()
+	profilePhoto = serializers.ImageField(required = False)
+
+	def get_cleaned_data(self):
+		super(MyCustomUserRegistrationSerializer, self).get_cleaned_data()
+		return {
+			'password1': self.validated_data.get('password1', ''),
+			'password2': self.validated_data.get('password2', ''),
+			'email': self.validated_data.get('email', ''),
+			'firstName': self.validated_data.get('firstName', ''),
+			'lastName': self.validated_data.get('lastName', ''),
+			'profilePhoto' : self.validated_data.get('profilePhoto',''),
+		}
+
+	def save(self, request):
+		user = super().save(request)
+		user.firstName = self.data.get('firstName')
+		user.lastName = self.data.get('lastName')
+		user.email = self.data.get('email')
+		user.profilePhoto = self.data.get('profilePhoto')
+		user.save()
+		return user
